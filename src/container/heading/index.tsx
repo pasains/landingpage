c7ab7558a9d72a9e2855c47a0c097ce092504663
Susilawati <susilawatibudiutami@gmail.com>
2024-05-28 04:33:42 +0700

    feat: header and footer layout

--- src/container/heading/index.tsx
+++ src/container/heading/index.tsx
@@ -2,24 +2,16 @@ import { NavBar } from "../navbar";
 
 export function Heading() {
   return (
-    <div className="flex-no-wrap flex w-full z-9999 h-[120px] bg-white fixed place-content-center top-0">
+    <div className="flex max-w-full justify-between items-center p-[25px] bg-white mx-auto font-nunito container fixed top-0">
       <div>
         <img
           src={require("../../image/logo.png")}
           alt="logopasains"
-          className="size-xsm my-[15px]"
+          className="size-xsm"
         />
       </div>
-      <div className="my-auto">
+      <div>
         <NavBar />
-      </div>
-      <div className="flex flex-row my-auto">
-        <button className="ml-12 mr-6 w-24 h-14 bg-light-orange rounded-md text-md transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 hover:bg-bold-orange duration-300">
-          Sign up
-        </button>
-        <button className="w-24 h-14 bg-light-orange rounded-md text-md transition ease-in-out delay-15 hover:-translate-y-1 hover:scale-110 hover:bg-bold-orange duration-300">
-          Log in
-        </button>
       </div>
     </div>
   );
--- src/pages/kepengurusan.tsx
+++ src/pages/kepengurusan.tsx
@@ -1,21 +1,14 @@
-import { Bottom } from "../container/bottom";
-import { Heading } from "../container/heading";
 import { KepengurusanContent } from "../content/kepengurusan.content";
+import { Layout } from "../layout";
 
 export function Kepengurusan() {
   return (
-    <div>
-      <div>
-        <Heading />
-      </div>
-      <div className="flex flex-col pt-[120px]">
+    <Layout>
+      <div className="flex flex-col pt-[120px] mb-[50px]">
         <div>
           <KepengurusanContent />
         </div>
-        <div className="mt-12">
-          <Bottom />
-        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
