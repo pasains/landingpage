5412ca8ac1df3e792b23c08fb4eb962a67f0c828
suhada <suhada@pts.sc>
2024-06-01 07:20:56 +0700

    feat: add responsive footer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,28 @@
 import React from "react";
-import {Layout} from "../layout";
+import { Layout } from "../layout";
 import { NavLink } from "react-router-dom";
-import useHome from "../hooks/home/index"
+import useHome from "../hooks/home/index";
 
 export function Home() {
-const { totalPages, nextPage, prevPage, title, page} = useHome();
+  const { totalPages, nextPage, prevPage, title, page } = useHome();
 
   return (
     <Layout>
       <div className="font-nunito">
         <div className="pt-[90px] max-w-full justify-items-center mx-auto text-center">
           <div className="bg-gunung bg-center place-content-center h-[640px] mx-auto max-w-full">
-          <div className="bg-black bg-opacity-40 h-[640px] mx-auto place-content-center max-w-full">
-            <h1 className="text-2xl text-white font-bold leading-tight tracking-widest">
-              NEVER ENDING <br />
-              <span> BROTHERHOOD</span>
-            </h1>
+            <div className="bg-black bg-opacity-40 h-[640px] mx-auto place-content-center max-w-full">
+              <h1 className="text-2xl text-white font-bold leading-tight tracking-widest">
+                NEVER ENDING <br />
+                <span> BROTHERHOOD</span>
+              </h1>
+            </div>
           </div>
-          </div>
-          <div className="contaier border mt-[50px] p-[25px] w-[600px] text-md text-justify mx-auto tracking-wide">
-          Selamat datang di situs web resmi PASAINS, wadah bagi para mahasiswa dan mahasiswi Fakultas Matematika dan Ilmu Pengetahuan Alam Universitas Gadjah Mada
-          yang memiliki semangat tinggi dalam menjaga dan menjelajahi keindahan alam.
+          <div className="contaier border mt-[50px] p-[25px] md:w-[600px] text-md text-justify mx-auto tracking-wide">
+            Selamat datang di situs web resmi PASAINS, wadah bagi para mahasiswa
+            dan mahasiswi Fakultas Matematika dan Ilmu Pengetahuan Alam
+            Universitas Gadjah Mada yang memiliki semangat tinggi dalam menjaga
+            dan menjelajahi keindahan alam.
           </div>
           <div className="text-xl font-bold py-[50px] text-bold-orange">
             KEGIATAN TERBARU
@@ -53,16 +55,17 @@ const { totalPages, nextPage, prevPage, title, page} = useHome();
               );
             })}
           </div>
-          </div>
-          <div className="items-center mx-auto font-light text-md text-center mb-[50px] space-x-md">
-            <button onClick={prevPage} disabled={page === 1}>
-              Prev
-            </button>
-            <button>{page}</button>
-            <button onClick={nextPage} disabled={page === totalPages}>
-              Next
-            </button>
-          </div>
-          </div>
+        </div>
+        <div className="items-center mx-auto font-light text-md text-center mb-[50px] space-x-md">
+          <button onClick={prevPage} disabled={page === 1}>
+            Prev
+          </button>
+          <button>{page}</button>
+          <button onClick={nextPage} disabled={page === totalPages}>
+            Next
+          </button>
+        </div>
+      </div>
     </Layout>
-  )};
+  );
+}
