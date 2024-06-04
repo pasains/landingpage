d800873731f7771d27684201202458e7cc7d9cd7
Swandono <me@swandono.com>
2024-06-04 22:20:13 +0700

    fix: dotenv error

--- webpack.config.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  resolve: {
-    fallback: {
-      "os": require.resolve("os-browserify/browser"),
-      "path": require.resolve("path-browserify"),
-      "crypto": require.resolve("crypto-browserify")
-    }
-  }
-};
